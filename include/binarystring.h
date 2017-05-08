a8b75929a1877f6fea2e5b4a25bda3ff8af264d6
nkdhny <golomedov@gmail.com>
2017-05-08 12:43:52 +0300

    + binary string with feature hashing

--- include/binarystring.h
+++ include/binarystring.h
@@ -12,6 +12,23 @@ namespace yasda {
     using BinaryString = std::vector<uint64_t>;
     using SparseBinaryString = std::unordered_set<size_t>;
 
+    struct HashedSparseBinaryString {
+        const size_t hashBits_;
+        const size_t hashValue_;
+        std::unordered_set<size_t> triggeredBits_;
+
+        explicit HashedSparseBinaryString(size_t hashBits=18) :
+            hashBits_(hashBits),
+            hashValue_(1 << hashBits) { }
+
+        inline size_t hash(size_t value) const {
+            return value % hashValue_;
+        }
+
+        HashedSparseBinaryString(const HashedSparseBinaryString&) = delete;
+        HashedSparseBinaryString& operator =(const HashedSparseBinaryString&) = delete;
+    };
+
     template <typename T> bool GetBit(const T& bstr, size_t bitId);
     template <typename T> void SetBit(T& bstr, size_t bitId, bool value);
     template <typename T> bool BinaryStringsAreSame(const T& left, const T& right);
